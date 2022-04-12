b435dbe0b622c7b1997051c01ed031009602553e
José Reyes <79488817+josereyg21@users.noreply.github.com>
2022-04-11 18:55:50 -0600

    update

---
+++ Lists.py
@@ -0,0 +1,16 @@
+
+dct = {
+    "insert": apd,
+    "append": ins,
+    "sort": srt,
+    "reverse": rev,
+    "remove": rem,
+    "print": prt
+}
+
+lst = []
+x = 0
+while x != int(input()):
+    x += 1
+
+print(x)
